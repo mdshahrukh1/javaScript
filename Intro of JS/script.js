19b6c482583de616fe1a88a064d923ed60ee104d
mdshahrukh1 <156064452+mdshahrukh1@users.noreply.github.com>
2025-02-03 14:41:00 +0530

    Add files via upload

---
+++ Intro of JS/script.js
@@ -0,0 +1,10 @@
+document.getElementById("p1").innerHTML = "Hello JavaScript";
+
+document.getElementById('p1').style.color='red';
+
+document.getElementById('intro').style.fontSize='35px';
+
+document.body.style.backgroundColor = "lightyellow";
+
+
+
---
+++ Intro of JS/script1.js
@@ -0,0 +1,18 @@
+document.body.style.backgroundColor = "Lightyellow";
+
+document.getElementById("heading").innerHTML = "JavaScript";
+
+document.getElementById('heading').style.fontSize = "35px";
+
+document.getElementById("p1").innerHTML = "JavaScript is a powerful, flexible, and fast programming language.";
+
+document.getElementById("p2").innerHTML = "JavaScript powers the dynamic behavior on websites.";
+
+document.getElementById("p3").innerHTML = "JavaScript remains at the core of web devlopment.";
+
+document.getElementById('p1').style.color = "red";
+
+document.getElementById('p2').style.color = "blue";
+
+document.getElementById('p3').style.color = "green";
+
